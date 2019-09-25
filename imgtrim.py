95990bfc053246f1347c993bef7c0e4dbd6d3e6a
mc.android.developer <mc.android.developer@gmail.com>
2019-09-25 00:26:57 -0700

    Create basic project

---
+++ imgtrim.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os
+import fnmatch
+
+import imgtrim.rename
+
+def print_files(files_list):
+    print('Files to trim:')
+    for f in files_list:
+        print('\t' + f)
+
+
+def main():
+    print('=== ImgTrim =====')
+    work_dir = os.path.dirname(os.path.realpath(__file__))
+    print('Seek files in dir: ' + work_dir)
+
+    img_files = []
+    img_extensions = ['*.jpg', '*.jpeg', '*.png']
+    for root, dirs, files in os.walk(work_dir):
+        for file in files:
+            for ext in img_extensions:
+                if fnmatch.fnmatch(file, ext):
+                    img_files.append(os.path.join(root, file))
+
+    print_files(img_files)
+
+
+if __name__ == "__main__":
+    main()
