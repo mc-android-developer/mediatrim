326fa14f4c2435291200c37f43a04a4f112e868f
mc <mc.android.developer@gmail.com>
2019-11-30 23:39:05 -0800

    Do files renaming before exif data remove

--- mediatrim.py
+++ mediatrim.py
@@ -12,6 +12,17 @@ def print_files(files_list):
     for f in files_list:
         print('\t' + f)
 
+def get_media_files_in_dir(work_dir):
+    media_extensions = ['*.jpg', '*.jpeg', '*.png', '*.mp4', '*.avi']
+    media_files = []
+    for root, dirs, files in os.walk(work_dir):
+        for file in files:
+            for ext in media_extensions:
+                if fnmatch.fnmatch(file, ext):
+                    media_files.append(os.path.join(root, file))
+
+    return media_files
+
 
 def main():
     print('=== MediaTrim =====')
@@ -21,23 +32,17 @@ def main():
     mediatrim.remove_duplicates.remove_duplicates_in_dir_recursively(work_dir)
 
     print('Seek media files in dir: ' + work_dir)
-
-    media_files = []
-    media_extensions = ['*.jpg', '*.jpeg', '*.png', '*.mp4', '*.avi']
-    for root, dirs, files in os.walk(work_dir):
-        for file in files:
-            for ext in media_extensions:
-                if fnmatch.fnmatch(file, ext):
-                    media_files.append(os.path.join(root, file))
+    media_files = get_media_files_in_dir(work_dir)
 
     print('Found media files:')
     print_files(media_files)
 
-    print('Remove exif data:')
-    mediatrim.exif.remove_exif(media_files)
-
     print('Rename media files:')
     mediatrim.rename.rename_files(media_files)
+    media_files = get_media_files_in_dir(work_dir)
+
+    print('Remove exif data:')
+    mediatrim.exif.remove_exif(media_files)
 
 
 if __name__ == "__main__":
